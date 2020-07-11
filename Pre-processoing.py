ddd8f9f92e9550d6d21e92466f34ee9427f2ffe8
KEERTHAN MS <64530716+DOPEMAVERICK@users.noreply.github.com>
2020-07-11 18:22:01 +0530

    Update Pre-processoing.py

--- Pre-processoing.py
+++ Pre-processoing.py
@@ -1,17 +1,16 @@
 ## Pre-processing of data ##
 
 # IMPORT THE LIBRARY
-
 import pandas as pd               # GIVE ANY ALYAS NAME 
 
 # IMPORT THE DATASET
-
-data=pd.read_csv('DATA.csv')     # GIVE THE REQUIRED DATASET NAME WITH EXTENSION AS .CSV (COMMA SEPRATED VALUES)
+data=pd.read_csv('Data.csv')     # GIVE THE REQUIRED DATASET NAME WITH EXTENSION AS .CSV (COMMA SEPRATED VALUES)
 
 # CREATING THE X,Y ARRAY
-x=data.iloc[:,:-1].values        # HERE  :  MEANS ALL THE ROWS ACCEPT LAST COLUMN (according to my dataset)
-y=data.iloc[:,-1].values         
+x=data.iloc[:,:-1].values        # HERE  :  MEANS ALL THE ROWS :-1 ACCEPT LAST COLUMN (according to used dataset)
+y=data.iloc[:,-1].values         # HERE  :  MAENS ALL THE ROWS -1 FROM LAST COLUMN
 
-
-
+ 
+# CLEANING THE DATA SET (FIXING THE MISSING DATA IN SET)
+from sklearn.preprocessing import Imputer
  
