66dd1e2388fa79b06c96fa6e3ae737bfe3e9a45f
KEERTHAN MS <64530716+DOPEMAVERICK@users.noreply.github.com>
2020-07-11 17:23:13 +0530

    Update Pre-processoing.py

--- Pre-processoing.py
+++ Pre-processoing.py
@@ -7,7 +7,10 @@ import pandas as pd               # GIVE ANY ALYAS NAME
 # IMPORT THE DATASET
 
 data=pd.read_csv('DATA.csv')     # GIVE THE REQUIRED DATASET NAME WITH EXTENSION AS .CSV (COMMA SEPRATED VALUES)
- 
+
+# CREATING THE X,Y ARRAY
+x=data.iloc[:,:-1].values        # HERE  :  MEANS ALL THE ROWS ACCEPT LAST COLUMN (according to my dataset)
+y=data.iloc[:,-1].values         
 
 
 
